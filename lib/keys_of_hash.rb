d0f502edc2d246e97bfb755be5a1929eb68f1d55
Jake Bartlow <jtbartlow@gmail.com>
2019-03-28 22:21:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,17 @@ animals =
 
 class Hash
   def keys_of(*arguments)
-    arr = []
-    
+   array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
+    end
+    return array
   end
-end
+  
+end 
